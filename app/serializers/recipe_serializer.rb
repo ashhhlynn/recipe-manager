4f889b47fb75e2e1dc851b784a453d7ce5fa3e09
ashhhlynn <ashleylynnwelch@gmail.com>
2023-05-30 03:02:18 -0500

    test

--- app/serializers/recipe_serializer.rb
+++
@@ -1,6 +0,0 @@
-class RecipeSerializer < ActiveModel::Serializer
-    attributes :id, :name, :image_url, :description, :average, :recipe_ingredients, :reviews
-    has_many :reviews
-    has_many :recipe_ingredients
-    has_many :favorites
-  end
