ac3f21441752d9f047536db48b4736975ac4509b
ashhhlynn <ashleylynnwelch@gmail.com>
2023-08-25 20:47:26 -0500

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+c1 = Category.create(title: "appetizer")
+c2 = Category.create(title: "breakfast")
+c3 = Category.create(title: "main dish")
+c4 = Category.create(title: "side")
+c5 = Category.create(title: "salad")
+c6 = Category.create(title: "soup")
+c7 = Category.create(title: "dessert")
+c8 = Category.create(title: "beverage")
+c9 = Category.create(title: "other")
--- render-build.sh
+++ render-build.sh
@@ -9,7 +9,7 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
-
+bundle exec rails db:seed
 
 #if you have seeds to run add:
 # bundle exec rails db:seed
